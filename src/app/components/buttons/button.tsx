d29c2905816f2bf8c2f21d1e93fa3d75462af451
Thiago <thiagopinto.sena@gmail.com>
2024-06-05 22:58:00 -0300

    ajuste identação

--- src/app/components/buttons/button.tsx
+++ src/app/components/buttons/button.tsx
@@ -1,35 +1,32 @@
-import { Variantes, CoresFundo, CoresTexto, CoresBorda } from "./variantes";
+import { Variantes, CoresFundo, CoresTexto, CoresBorda } from './variantes';
 
 interface ButtonProps {
-	texto: string,
-	variante?: "default" | "rounded" | "outlined",
-	corTexto?: "primaria" | "secundaria" | "terciaria",
-	corFundo?: "primaria" | "secundaria" | "terciaria" | "transparente",
+  texto: string;
+  variante?: 'default' | 'rounded' | 'outlined';
+  corTexto?: 'primaria' | 'secundaria' | 'terciaria';
+  corFundo?: 'primaria' | 'secundaria' | 'terciaria' | 'transparente';
 }
 
-function Butao({ texto, variante = "default", corTexto = "terciaria", corFundo = "primaria" }: ButtonProps) {
+function Butao({ texto, variante = 'default', corTexto = 'terciaria', corFundo = 'primaria' }: ButtonProps) {
+  const padraoButao = 'flex w-[222px] h-[56px] px-[34px] justify-center items-center gap-[-2px] shrink-0';
+  const variacaoButao = Variantes[variante];
+  const variacaoCor = CoresTexto[corTexto];
+  const variacaoFundo = CoresFundo[corFundo];
 
-	const padraoButao = "flex w-[222px] h-[56px] px-[34px] justify-center items-center gap-[-2px] shrink-0";
-	const variacaoButao = Variantes[variante];
-	const variacaoCor = CoresTexto[corTexto];
-	const variacaoFundo = CoresFundo[corFundo];
+  var variacaoBorda = '';
+  var ativado = false;
 
-	var variacaoBorda = "";
-	var ativado = false;
+  if (variante == 'outlined') {
+    variacaoBorda = CoresBorda[corTexto];
+  }
 
-	if (variante == "outlined") {
-		variacaoBorda = CoresBorda[corTexto];
-	}
-
-	return (
-		<>
-			<button
-				className={`${padraoButao} ${variacaoButao} ${variacaoCor} ${variacaoFundo} ${variacaoBorda}`}
-			>
-				{texto}
-			</button>
-		</>
-	);
+  return (
+    <>
+      <button className={`${padraoButao} ${variacaoButao} ${variacaoCor} ${variacaoFundo} ${variacaoBorda}`}>
+        {texto}
+      </button>
+    </>
+  );
 }
 
 export default Butao;
