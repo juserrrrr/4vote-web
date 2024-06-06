dd0b07ff1736d6f92c2691621b17a89f4fc5bb8d
Fernanda-Marinho <98289412+Fernanda-Marinho@users.noreply.github.com>
2024-06-05 22:39:42 -0300

    Revert "Ts/task001"

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,6 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  mode: 'jit',
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -13,10 +12,6 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-      colors: {
-        corPrincipal: '#052A76',
-        corSecundaria: '#02A6F4',
       },
     },
   },
