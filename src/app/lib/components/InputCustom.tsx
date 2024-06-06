d803dadfcc70f5c6ed434b06349e24963fd6659e
José Gabriel <gabriel-pontes09@hotmail.com>
2024-06-05 21:27:49 -0300

    feat: Update editor settings, dependencies, and tailwind configuration

--- src/app/lib/components/InputCustom.tsx
+++ src/app/lib/components/InputCustom.tsx
@@ -1,4 +1,5 @@
-import { InputHTMLAttributes, forwardRef } from 'react';
+'use client';
+import React, { InputHTMLAttributes, forwardRef, useState } from 'react';
 
 interface InputCustomProps extends InputHTMLAttributes<HTMLInputElement> {
   label: string;
@@ -8,12 +9,31 @@ const InputCustom = forwardRef<HTMLInputElement, InputCustomProps>(function Inpu
   { label, ...restProps }: InputCustomProps,
   ref,
 ) {
+  const [isFocused, setisFocused] = useState<Boolean>(false);
+  const [hasContent, sethasContent] = useState<Boolean>(false);
+
+  const handleFocus = () => {
+    setisFocused(true);
+  };
+
+  const handleBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+    setisFocused(false);
+    sethasContent(!!e.target.value);
+  };
+
   return (
-    <div className="flex flex-col w-full p-10">
-      <label className="text-blue-950 font-bold">{label}</label>
+    <div className="flex flex-col w-full p-1 justify-center h-20">
+      <label
+        className={`ml-2 absolute transition-all duration-200 text-blue-950 font-bold text-md 
+        ${isFocused || hasContent ? 'ml-1 transform -translate-y-7 text-sm' : ''}`}
+      >
+        {label}
+      </label>
       <input
-        className="p-2 rounded-lg text-blue-950 border-4  border-solid border-blue-950 focus:outline-none focus:border-blue-900"
+        className="p-2 rounded-lg text-blue-950 border-2  border-solid border-blue-950 focus:outline-none focus:border-blue-900"
         ref={ref}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
         {...restProps}
       />
     </div>
