614082e3c80683bd0de27bdc7d63b8ee0d8abe97
José Gabriel <gabriel-pontes09@hotmail.com>
2024-06-05 21:45:01 -0300

    refactor: Update InputCustom component to include helper text

--- src/app/lib/components/InputCustom.tsx
+++ src/app/lib/components/InputCustom.tsx
@@ -1,16 +1,18 @@
 'use client';
-import React, { InputHTMLAttributes, forwardRef, useState } from 'react';
+import React, { InputHTMLAttributes, forwardRef, useId, useState } from 'react';
 
 interface InputCustomProps extends InputHTMLAttributes<HTMLInputElement> {
-  label: string;
+  label?: string;
+  helperText?: string;
 }
 
 const InputCustom = forwardRef<HTMLInputElement, InputCustomProps>(function InputCustom(
-  { label, ...restProps }: InputCustomProps,
+  { label = '', helperText = '', ...restProps }: InputCustomProps,
   ref,
 ) {
   const [isFocused, setisFocused] = useState<Boolean>(false);
   const [hasContent, sethasContent] = useState<Boolean>(false);
+  const inputId = useId();
 
   const handleFocus = () => {
     setisFocused(true);
@@ -23,19 +25,21 @@ const InputCustom = forwardRef<HTMLInputElement, InputCustomProps>(function Inpu
 
   return (
     <div className="flex flex-col w-full p-1 justify-center h-20">
-      <label
-        className={`ml-2 absolute transition-all duration-200 text-blue-950 font-bold text-md 
-        ${isFocused || hasContent ? 'ml-1 transform -translate-y-7 text-sm' : ''}`}
-      >
-        {label}
-      </label>
       <input
-        className="p-2 rounded-lg text-blue-950 border-2  border-solid border-blue-950 focus:outline-none focus:border-blue-900"
+        id={inputId}
+        className="p-2 h-10 rounded-md text-blue-950 border-2  border-solid border-blue-950 focus:outline-none focus:border-blue-900"
         ref={ref}
         onFocus={handleFocus}
         onBlur={handleBlur}
         {...restProps}
       />
+      <label
+        htmlFor={inputId}
+        className={`ml-2 absolute transition-all duration-200 text-blue-950 font-bold text-md pointer-events-none 
+        ${isFocused || hasContent ? 'ml-1 transform -translate-y-7 text-sm pointer-events-auto' : 'pointer-events-none '}`}
+      >
+        {label}
+      </label>
     </div>
   );
 });
