c72902d188e155b88a3a6392f95af9eba2e3e8f3
LuisMarioRC <luismariocarneiro8@gmail.com>
2024-06-09 21:41:13 -0300

    feat: Criando filtro de opção
    Co-authored-by: Gabriel Henry <GHenryssg@users.noreply.github.com>

---
+++ src/app/checkbox.tsx
@@ -0,0 +1,20 @@
+'use client';
+import React from 'react';
+import Butao from './components/buttons/button';
+import FilterCheckbox from '@/app/components/checkbox/checkbox';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      {' '}
+      <p>Filtros:</p>
+      <FilterCheckbox texto="Criadas por mim" />
+      <FilterCheckbox texto="Que participo" />
+      <FilterCheckbox texto="Arquivadas" />
+      <FilterCheckbox texto="Encerradas" />
+      <Butao texto="Aplicar" />
+    </div>
+  );
+};
+
+export default Home;
