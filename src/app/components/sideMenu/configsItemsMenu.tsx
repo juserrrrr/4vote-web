5a10c3b0e55cf4567e6ff9fee6e91ab7fddd9a6c
José Gabriel <gabriel-pontes09@hotmail.com>
2024-06-13 15:17:46 -0300

    refactor side menu component and update text in configsItemsMenu

--- src/app/components/sideMenu/configsItemsMenu.tsx
+++ src/app/components/sideMenu/configsItemsMenu.tsx
@@ -8,7 +8,7 @@ const configMenuItems = {
 export const menuItems = [
   {
     href: '/inicio',
-    text: 'Início Teste',
+    text: 'Início',
     icon: <HomeIcon {...configMenuItems} />,
   },
   {
