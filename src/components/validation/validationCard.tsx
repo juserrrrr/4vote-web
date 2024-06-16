09c4e007c0b741945a318ca1e9f596f20e2501b7
Naylane Ribeiro <naylaneribeiro4@gmail.com>
2024-06-15 21:17:56 -0300

    feat: criando o componente de validação

---
+++ src/components/validation/validationCard.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import iconCorrect from './icons/check-circle.png';
+import iconIncorrect from './icons/alert-circle.png';
+import Image from 'next/image';
+
+interface ValidacaoProps {
+  answer: boolean;
+}
+
+export const ValidationCard: React.FC<ValidacaoProps> = ({ answer }) => {
+  const fontStyle = "text-center text-sky-900 text-[29px] font-bold font-['Open Sans']";
+
+  // Função para definir o componente de acordo com a resposta
+  const render = () => {
+    if (answer === true) {
+      return (
+        <>
+          <Image
+            src={iconCorrect}
+            alt="Icon"
+          />
+          <div className={fontStyle}>VOTO VÁLIDO</div>
+        </>
+      );
+    } else if (answer === false) {
+      return (
+        <>
+          <Image
+            src={iconIncorrect}
+            alt="Icon"
+          />
+          <div className={fontStyle}>VOTO INVÁLIDO</div>
+        </>
+      );
+    }
+    return null;
+  };
+
+  return (
+    <div className="w-[765px] h-[300px] bg-white rounded-[7px] flex-col justify-center items-center gap-2 inline-flex">
+      <div className="w-[765px] h-3.5 bg-sky-900 rounded-tl-[7px] rounded-tr-[7px]" />
+      {render()}
+      <div className="w-[765px] h-3.5 bg-sky-900 rounded-tl-[7px] rounded-tr-[7px] rotate-180" />
+    </div>
+  );
+};
