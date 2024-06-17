b3e8d395574df1bb86eb22c0a1cf65e33b6a6744
Ilson <ilsonetoneto06@gmail.com>
2024-06-17 19:25:52 -0300

    refactor: Ajustando inputs e botões do Modal para adequar-se ao projeto

--- src/app/components/modal/modal.tsx
+++ src/app/components/modal/modal.tsx
@@ -1,7 +1,8 @@
 import React, { useId, useState } from 'react';
-import ReactModal from 'react-modal';
+import InputCustom from '@/components/InputCustom/InputCustom';
+import Butao from '@/components/buttons/button';
 
-interface status {
+interface ModalInterface {
   isOpennow: boolean;
   closeModal: () => void;
   title: string;
@@ -9,50 +10,55 @@ interface status {
   instruction: string;
 }
 
-export default function Modal({ isOpennow, closeModal, title, subtitle, instruction }: status) {
+const Modal: React.FC<ModalInterface> = ({ isOpennow, closeModal, title, subtitle, instruction }) => {
   const id = useId();
+  if (!isOpennow) return null;
 
   return (
-    <ReactModal
-      isOpen={isOpennow}
-      onRequestClose={closeModal}
-      className={
-        'bg-corNeutro w-[750px] h-[310px] rounded-md border-corPrincipal border-2 text-center text-corPrincipal flex flex-col items-center justify-center font-sans relative '
-      }
-      overlayClassName="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center"
+    <div
+      id="overlay"
+      className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center"
     >
-      {' '}
-      <div className="font-bold text-center">
-        <h1 className={'mb-0  text-[32px] -mt-20'}>{title}</h1>
-        <h2 className={'mb-8 text-[12px] w-full'}>{subtitle}</h2>
-      </div>
-      <div className="w-[394px]">
-        <p className="text-left text-[14px] -mb-3 font-inter font-bold">{instruction}</p>
-        <input
-          id="id"
-          className={
-            'p-2 h-10 mt-3 rounded-md w-[394px]  border-2  border-solid border-blue-950 focus:outline-none focus:border-blue-900 bg-corPrincipal font-inter font-bold text-corNeutro text-[22px]'
-          }
-        ></input>
-      </div>
-      <div className="absolute bottom-4 right-10 flex space-x-4">
-        <React.Fragment>
-          <button
-            onClick={closeModal}
-            className={'text-corErro text-[15px] font-bold'}
-          >
-            Sair
+      <div
+        className={
+          'bg-corNeutro w-[750px] h-[310px] rounded-md border-corPrincipal border-2 text-center text-corPrincipal flex flex-col items-center justify-center font-sans relative '
+        }
+      >
+        {' '}
+        <div className="font-bold text-center">
+          <h1 className={'mb-0  text-[32px] -mt-20'}>{title}</h1>
+          <h2 className={'mb-8 text-[12px] w-full'}>{subtitle}</h2>
+        </div>
+        <div className="w-[394px] font-sans font-bold">
+          <p className="text-left text-[14px] -mb-3 pl-1">{instruction}</p>
+          <InputCustom
+            id="id"
+            className="p-2 h-10 mt-3 rounded-[7px] w-full  border-2 border-blue-950 focus:outline-none bg-corPrincipal text-corNeutro text-[22px]"
+          ></InputCustom>
+        </div>
+        <div className="absolute bottom-4 right-8 flex justify-end items-end flex-1 self-stretch font-sans">
+          <button onClick={closeModal}>
+            <Butao
+              texto={'SAIR'}
+              variant="rounded"
+              textColor="primaria"
+              bgColor="transparente"
+              className="text-corErro text-[15px] flex w-[120px] h-[37px] py-0 pr-58 px-[38px] justify-center items-center font-bold gap-0.5 pl-1"
+            ></Butao>
           </button>
-          <button
-            onClick={closeModal}
-            className={
-              'text-corPrincipal border-2 border-corPrincipal rounded-[7px] text-[15px] font-bold p-1 flex-row justify-center '
-            }
-          >
-            Confirmar
+          <button onClick={closeModal}>
+            <Butao
+              texto={'CONFIRMAR'}
+              variant="outlined"
+              textColor="terciaria"
+              bgColor="transparente"
+              className="w-[120px] h-[37px] text-corPrincipal font-bold font-sans"
+            ></Butao>
           </button>
-        </React.Fragment>
+        </div>
       </div>
-    </ReactModal>
+    </div>
   );
-}
+};
+
+export default Modal;
