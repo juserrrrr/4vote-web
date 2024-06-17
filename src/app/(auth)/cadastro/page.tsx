81d704eb7d6511b36942e861f5023befcfced82f
Thiago <thiagopinto.sena@gmail.com>
2024-06-16 22:50:07 -0300

    Feature: criação da página cadastro

---
+++ src/app/(auth)/cadastro/page.tsx
@@ -0,0 +1,27 @@
+import InputCustom from '@/components/InputCustom/InputCustom';
+import Butao from '@/components/buttons/button';
+import Link from 'next/link';
+
+export default function Cadastro() {
+  return (
+    <>
+      <h2 className="text-corPrincipal text-center font-bold text-5xl">CADASTRO</h2>
+      <div className="w-full flex flex-col justify-between items-center flex-shrink-0 px-10 gap-10">
+        <div className="w-full flex flex-col justify-between gap-5">
+          <InputCustom label="Nome" />
+          <InputCustom label="Email" />
+          <InputCustom label="CPF" />
+          <InputCustom label="Senha" />
+          <InputCustom label="Confirmar Senha" />
+        </div>
+        <div className="w-full">
+          <Butao
+            texto="Confirmar Cadastro"
+            variant="rounded"
+            className="w-full"
+          />
+        </div>
+      </div>
+    </>
+  );
+}
