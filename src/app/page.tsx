d0cb38ddcbc4c973fa2590945bcbfea7641af355
Sara Ferreira <sara2004.souza@gmail.com>
2024-06-18 10:54:19 -0300

    Consertando erros do merge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-<<<<<<< HEAD
 import ResponsePage from './responsePage/page';
 
 export default function HomePage() {
@@ -14,15 +13,5 @@ export default function HomePage() {
       imageUrl="https://picsum.photos/300/104"
       question="Quem você escolhe para ser o novo Diretório Acadêmico do curso?"
     />
-=======
-import SquareQuestion from '@/components/elementsEnqVot/SquareQuestion';
-
-export default function HomePage() {
-  return (
-    <div className="flex bg-gray-300 justify-center items-center h-screen">
-      <SquareQuestion
-      />
-    </div>
->>>>>>> 2a754cfca4dbd16b42fa02ae6979db45153292cc
   );
 }
