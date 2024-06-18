54a1ef2d7562aafd804371c2142630a9cbd89c12
GHenryssg <gabrielhenrysilva10@gmail.com>
2024-06-17 21:36:40 -0300

    feat: Criação da tela de histórico

--- src/app/(aplicacao)/historico/page.tsx
+++ src/app/(aplicacao)/historico/page.tsx
@@ -2,23 +2,95 @@
 import React, { useState } from 'react';
 import SearchBar from '@/components/search/Searchbar';
 import FilterButton from '@/components/buttonFilter/FilterButton';
-import './styles.css';
-import SideMenu from '@/components/sideMenu/SideMenu';
-import { Header } from '@/components/header/header';
+
+function CardFake({ key }: { key: string }) {
+  return (
+    <div
+      key={key}
+      className="w-[300px] h-[250px] bg-corPrincipal"
+    ></div>
+  );
+}
+
+const cards = [
+  {
+    title: 'Enquete 1',
+    description: 'Descrição da enquete 1',
+  },
+  {
+    title: 'Enquete 2',
+    description: 'Descrição da enquete 2',
+  },
+  {
+    title: 'Enquete 3',
+    description: 'Descrição da enquete 3',
+  },
+  {
+    title: 'Enquete 4',
+    description: 'Descrição da enquete 4',
+  },
+  {
+    title: 'Enquete 5',
+    description: 'Descrição da enquete 5',
+  },
+  {
+    title: 'Enquete 6',
+    description: 'Descrição da enquete 6',
+  },
+  {
+    title: 'Enquete 7',
+    description: 'Descrição da enquete 7',
+  },
+  {
+    title: 'Enquete 8',
+    description: 'Descrição da enquete 8',
+  },
+  {
+    title: 'Enquete 9',
+    description: 'Descrição da enquete 9',
+  },
+  {
+    title: 'Enquete 10',
+    description: 'Descrição da enquete 9',
+  },
+  {
+    title: 'Enquete 10',
+    description: 'Descrição da enquete 9',
+  },
+  {
+    title: 'Enquete 10',
+    description: 'Descrição da enquete 9',
+  },
+  {
+    title: 'Enquete 10',
+    description: 'Descrição da enquete 9',
+  },
+  {
+    title: 'Enquete 10',
+    description: 'Descrição da enquete 9',
+  },
+];
 
 export default function HomePage() {
-  const [text, setText] = useState<string>('');
-  console.log(text);
-
   return (
     <div className="main">
-      <header />
       <main>
-        <SearchBar />
-        <div className="full-screen-container">
-          <div className="filter-buttons">
+        <div className="flex flex-col justify-center tems-center px-16 py-6 gap-6 text-corPrincipal">
+          <div className="flex flex-col w-full justify-start">
+            <h1 className="text-4xl font-bold">Histórico</h1>
+            <h2 className="text-4x1 font-sans">
+              Visualize as enq./vot. que você criou ou participa, tanto públicas quanto privadas.
+            </h2>
+          </div>
+          <SearchBar />
+          <div className="w-full flex flex-row items-center gap-8">
             <FilterButton variante="ordenar" />
             <FilterButton variante="filtrar" />
+          </div>
+          <div className="flex flex-row flex-wrap content-between">
+            {cards.map((card, index) => (
+              <CardFake key={String(index)} />
+            ))}
           </div>
         </div>
       </main>
