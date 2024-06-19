2e4ba0043c9794d2e7e0cf6a5b12bd0715dde73b
Thiago <thiagopinto.sena@gmail.com>
2024-06-18 23:13:29 -0300

    Update: ajuste tailwind className Cadastro

--- src/app/(auth)/cadastro/page.tsx
+++ src/app/(auth)/cadastro/page.tsx
@@ -10,41 +10,43 @@ export const metadata: Metadata = {
 export default function Cadastro() {
   return (
     <>
-      <h2 className="text-corPrincipal text-center font-bold text-5xl">CADASTRO</h2>
-      <div className="w-full flex flex-col justify-between items-center flex-shrink-0 px-10 gap-10">
-        <div className="w-full flex flex-col justify-between gap-3">
-          <InputCustom
-            label="Nome"
-            helperText="Campo Obrigatório"
-            error={true}
-          />
-          <InputCustom
-            label="Email"
-            helperText="Campo Obrigatório"
-            error={true}
-          />
-          <InputCustom
-            label="CPF"
-            helperText="Campo Obrigatório"
-            error={true}
-          />
-          <InputCustom
-            label="Senha"
-            helperText="Campo Obrigatório"
-            error={true}
-          />
-          <InputCustom
-            label="Confirmar Senha"
-            helperText="Campo Obrigatório"
-            error={true}
-          />
-        </div>
-        <div className="w-full">
-          <Butao
-            texto="Confirmar Cadastro"
-            variant="rounded"
-            className="w-full"
-          />
+      <div className="h-full w-full flex flex-col justify-between items-center">
+        <h2 className="text-corPrincipal text-center font-bold text-5xl">CADASTRO</h2>
+        <div className="w-full flex flex-col justify-between items-center flex-shrink-0 px-10 gap-10">
+          <div className="w-full flex flex-col justify-between gap-3">
+            <InputCustom
+              label="Nome"
+              helperText="Campo Obrigatório"
+              error={true}
+            />
+            <InputCustom
+              label="Email"
+              helperText="Campo Obrigatório"
+              error={true}
+            />
+            <InputCustom
+              label="CPF"
+              helperText="Campo Obrigatório"
+              error={true}
+            />
+            <InputCustom
+              label="Senha"
+              helperText="Campo Obrigatório"
+              error={true}
+            />
+            <InputCustom
+              label="Confirmar Senha"
+              helperText="Campo Obrigatório"
+              error={true}
+            />
+          </div>
+          <div className="w-full">
+            <Butao
+              texto="Confirmar Cadastro"
+              variant="rounded"
+              className="w-full"
+            />
+          </div>
         </div>
       </div>
     </>
