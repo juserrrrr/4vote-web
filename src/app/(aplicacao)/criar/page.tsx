e2d9592a6430a21182b4e9f6baedf328603d5640
Sara Ferreira <sara2004.souza@gmail.com>
2024-06-25 18:24:30 -0300

    consertando erros

--- src/app/(aplicacao)/criar/page.tsx
+++ src/app/(aplicacao)/criar/page.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import { renderToPipeableStream } from 'react-dom/server';
 import SquareInfos from '@/components/elementsEnqVot/SquareInfos';
 import Butao from '@/components/buttons/button';
 import SquareOptions from '@/components/elementsEnqVot/SquareOptions';
 
 interface CreatePageProps {
-  title?: 'CONFIGURAÇÃO DA ENQUENTE' | 'CONFIGURAÇÃO DA VOTAÇÃO';
+  title?: 'CONFIGURAÇÃO DA ENQUETE' | 'CONFIGURAÇÃO DA VOTAÇÃO';
 }
-const CreatePage: React.FC<CreatePageProps> = ({ title = 'CONFIGURAÇÃO DA ENQUENTE' }) => {
+
+const CreatePage: React.FC<CreatePageProps> = () => {
+  const title = 'CONFIGURAÇÃO DA VOTAÇÃO';
   const backgraund = 'flex flex-col w-[1438px] h-[922px] bg-gray-300 justify-center items-center';
   const container1 = 'w-[1260px] h-[825px] p-3 mb-10';
   const container2 = 'w-[350px] h-[10px] absolute right-0 mt-10 p-5';
