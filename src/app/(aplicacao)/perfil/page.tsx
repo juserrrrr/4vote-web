14a239a279fef533cc260c63a7608545e4d49c8f
José Gabriel <89746639+juserrrrr@users.noreply.github.com>
2024-06-25 14:55:27 -0300

    feat: refactor profile page layout

--- src/app/(aplicacao)/perfil/page.tsx
+++ src/app/(aplicacao)/perfil/page.tsx
@@ -1,12 +1,11 @@
 import { EyeSlashIcon, PencilIcon } from '@heroicons/react/24/solid';
-import Head from 'next/head';
 import Image from 'next/image';
 
 export default function Home() {
   return (
     <div>
       <div className="flex justify-center items-center w-full h-[190px] bg-corPrincipal border-none my-5">
-        <div className="absolute top-20 relative inline-block">
+        <div className="top-20 relative inline-block">
           <div className="absolute top-28 left-28 bg-white p-2 rounded-full">
             <PencilIcon className="text-corPrincipal w-6" />
           </div>
