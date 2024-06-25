37667b383696e35959a3666eb6444d073d27f987
Pedro Mendes <pmo2002@gmail.com>
2024-06-25 05:50:08 -0300

    PM/Task045 - Lógica da tela de cadastro e login concluída,lib com algumas funções importantes criada

--- src/app/(aplicacao)/inicio/page.tsx
+++ src/app/(aplicacao)/inicio/page.tsx
@@ -1,7 +1,30 @@
+'use client';
+import { useEffect, useState } from 'react';
+import { axiosClient, getCookie, getCurrentUserId } from '../../../../lib';
+
 export default function Home() {
+  const [nomeUser, setNewNomeUser] = useState('Estou no início');
+
+  // Para rodar assim que a página iniciar
+  useEffect(() => {
+    const userId = getCurrentUserId();
+    const token = getCookie('accessToken');
+
+    if (userId != null) {
+      axiosClient
+        .get('/usuarios/' + userId, { headers: { Authorization: `Bearer ${token}` } })
+        .then((res) => {
+          setNewNomeUser('Bem-vindo ' + res.data.nome + '!');
+        })
+        .catch((err) => {
+          return null;
+        });
+    }
+  }, []);
+
   return (
     <>
-      <div className=" bg-corNeutro">Estou no início</div>
+      <h2 className=" bg-corNeutro">{nomeUser}</h2>
     </>
   );
 }
