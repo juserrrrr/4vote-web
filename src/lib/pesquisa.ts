6d4a2571400016310ab304c69bfffca7847e9623
Thiago Pinto Pereira Sena <108939221+ThiagoPPSena@users.noreply.github.com>
2024-06-25 15:01:32 -0300

    Ts/task axios (#35)
    * Criação Branch Axios
    
    * Criação de métodos de requisição da api para criar pesquisa, visualizar todas pesquisas e arquivar pesquisas
    
    * Finalização base das funções de requisições da api
    
    * Feature: Criação de interface pesquisa e função para converter Date para ISO String
    
    * Update final
    
    * Update: Excluindo o delete

---
+++ src/lib/pesquisa.ts
@@ -0,0 +1,66 @@
+import { AxiosError, AxiosResponse } from 'axios';
+import api from './app';
+
+interface Pesquisa {
+  titulo: string;
+  codigo: string;
+  dataCriacao: Date;
+  dataTermino: Date;
+  ehPublico: boolean;
+  descricao?: string;
+  criador: number;
+  arquivado: boolean;
+  URLimagem?: string;
+  ehVotacao: boolean;
+}
+
+function DateToISOString(pesquisa: Pesquisa): { dataCriacao: string; dataTermino: string } {
+  return {
+    dataCriacao: pesquisa.dataCriacao.toISOString(),
+    dataTermino: pesquisa.dataTermino.toISOString(),
+  };
+}
+
+export function setArquivado(id: number) {
+  api
+    .patch(`pesquisas/arquivar/${id}`)
+    .then((response: AxiosResponse) => {
+      console.log(response.data);
+      return true;
+    })
+    .catch((error: AxiosError) => {
+      console.log(error);
+      return false;
+    });
+}
+
+export function findAllPesquisas() {
+  api
+    .get('pesquisas')
+    .then((response: AxiosResponse) => {
+      console.log(response.data);
+      return true;
+    })
+    .catch((error: AxiosError) => {
+      console.log(error);
+      return false;
+    });
+}
+
+export function createPesquisa(pesquisa: Pesquisa) {
+  const pesquisaComDatasISO = {
+    ...pesquisa,
+    ...DateToISOString(pesquisa),
+  };
+
+  api
+    .post('pesquisas', pesquisa)
+    .then((response: AxiosResponse) => {
+      console.log(response.data);
+      return true;
+    })
+    .catch((error: AxiosError) => {
+      console.log(error);
+      return false;
+    });
+}
