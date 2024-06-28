8156974bdd4d4517522cbf9c87aa4a670bef0182
Fernanda-Marinho <fmarinho1918@gmail.com>
2024-06-28 12:01:06 -0300

    refactor: change function in opcaovotada

--- src/lib/opcaovotada.ts
+++ src/lib/opcaovotada.ts
@@ -1,46 +1,24 @@
-import { AxiosError, AxiosResponse } from 'axios';
+import axios from 'axios';
 import api from './api';
 
-interface OpcaoVotada {
+export interface OpcaoVotada {
   id_opcao: number;
   id_voto: number;
 }
 
-export function findAllOpcaoVotada(): Promise<boolean> {
-  return api
-    .get('opcaovotada')
-    .then((response: AxiosResponse) => {
-      console.log(response.data);
-      return true;
-    })
-    .catch((error: AxiosError) => {
-      console.log(error);
-      return false;
-    });
+async function getById(id_opcao: number): Promise<OpcaoVotada | Error> {
+  try {
+    const { data } = await api.get('/opcaovotada/id', id_opcao);
+    if (data) return data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      return new Error(error.response?.data.message);
+    }
+    return new Error('Erro desconhecido');
+  }
+  return new Error('Erro ao tentar pegar o id de opção votada');
 }
 
-export function createOpcao(opcaovotada: OpcaoVotada): Promise<boolean> {
-  return api
-    .post('opcaovotada', opcaovotada)
-    .then((response: AxiosResponse) => {
-      console.log(response.data);
-      return true;
-    })
-    .catch((error: AxiosError) => {
-      console.log(error);
-      return false;
-    });
-}
-
-export function findById(opcao_id: number): Promise<OpcaoVotada | null> {
-  return api
-    .get(`opcaovotada/${opcao_id}`)
-    .then((response: AxiosResponse<OpcaoVotada>) => {
-      console.log(response.data);
-      return response.data;
-    })
-    .catch((error: AxiosError) => {
-      console.log(error);
-      return null;
-    });
-}
+export const authService = {
+  getById,
+};
