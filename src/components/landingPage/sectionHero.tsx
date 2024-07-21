fc58a0f9c5528401ce99a2703fad18b75c49de5c
Gabriel Baptista <gabriel.costa.baptista1@gmail.com>
2024-07-21 02:52:59 -0300

    update: ajusta responsividade

--- src/components/landingPage/sectionHero.tsx
+++ src/components/landingPage/sectionHero.tsx
@@ -6,9 +6,9 @@ export function SectionHero() {
   return (
     <section
       id="home"
-      className="min-w-full h-auto bg-background-word bg-no-repeat bg-center bg-cover flex justify-center items-center py-8"
+      className="min-w-full h-auto bg-gradient-to-r from-[#043c86] to-[#028fdd] flex justify-center items-center py-8"
     >
-      <div className="w-full max-w-[1200px] px-[15px] mx-auto flex flex-col lg:flex-row justify-between items-center">
+      <div className="w-full max-w-[1650px] px-[15px] mx-auto flex flex-col lg:flex-row justify-between items-center">
         <div className="flex-1 text-center lg:text-left max-w-[613px]">
           <h1 className="text-white text-4xl lg:text-5xl font-medium lg:ml-[55px] mt-[110px]">
             Está pronto para dar voz às suas opiniões e transformar sua visão em realidade?
@@ -31,13 +31,16 @@ export function SectionHero() {
           </div>
         </div>
         <div className="flex-1 flex justify-center mt-8 lg:mt-0 lg:ml-8">
-          <Image
-            src={conection}
-            alt="conection"
-            width={500}
-            height={500}
-            className="object-contain lg:object-cover lg:max-w-[500px] lg:max-h-[500px]"
-          />
+          <div className="w-full h-auto max-w-[600px] max-h-[600px] lg:max-w-[700px] lg:max-h-[700px]">
+            <Image
+              src={conection}
+              alt="conection"
+              layout="responsive"
+              width={650} // Define the intrinsic width
+              height={650} // Define the intrinsic height
+              className="object-contain"
+            />
+          </div>
         </div>
       </div>
     </section>
